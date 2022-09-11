4bd3a48f9ea4c96b6092b1a7a389dcf25e0bd29c
masaun <masanoriuno@gmail.com>
2022-09-11 17:52:25 +0900

    [SC]: Define the IWorldIdGatedVoucher.sol as a interface of the WorldIdGatedVoucher.sol

--- contracts/WorldIdGatedVoucher.sol
+++ contracts/WorldIdGatedVoucher.sol
@@ -8,64 +8,66 @@ import { IWorldID } from './worldcoin/interfaces/IWorldID.sol';
 
 import { FoodVoucherNFT } from "./FoodVoucherNFT.sol";
 
+import { IWorldIdGatedVoucher } from "./interfaces/IWorldIdGatedVoucher.sol";
+
 
 /**
  * @title - World ID gated Voucher contract
  */ 
-contract WorldIdGatedVoucher {
+contract WorldIdGatedVoucher is IWorldIdGatedVoucher {
     using ByteHasher for bytes;
 
     ///////////////////////////////////////////////////////////////////////////////
     ///                                  ERRORS                                ///
     //////////////////////////////////////////////////////////////////////////////
 
-    /// @notice Thrown when trying to create or update FoodVoucherProgram details without being the manager
-    error Unauthorized();
+    // /// @notice Thrown when trying to create or update FoodVoucherProgram details without being the manager
+    // error Unauthorized();
 
-    /// @notice Thrown when attempting to reuse a nullifier
-    error InvalidNullifier();
+    // /// @notice Thrown when attempting to reuse a nullifier
+    // error InvalidNullifier();
 
-    /// @notice Thrown when attempting to claim a non-existant FoodVoucherProgram
-    error InvalidFoodVoucherProgram();
+    // /// @notice Thrown when attempting to claim a non-existant FoodVoucherProgram
+    // error InvalidFoodVoucherProgram();
 
 
     ///////////////////////////////////////////////////////////////////////////////
     ///                                  EVENTS                                ///
     //////////////////////////////////////////////////////////////////////////////
 
-    /// @notice Emitted when an FoodVoucherProgram is created
-    /// @param foodVoucherProgramId The id of the foodVoucherProgram
-    /// @param foodVoucherProgram The foodVoucherProgram details
-    event FoodVoucherProgramCreated(uint256 foodVoucherProgramId, FoodVoucherProgram foodVoucherProgram);
+    // /// @notice Emitted when an FoodVoucherProgram is created
+    // /// @param foodVoucherProgramId The id of the foodVoucherProgram
+    // /// @param foodVoucherProgram The foodVoucherProgram details
+    // event FoodVoucherProgramCreated(uint256 foodVoucherProgramId, FoodVoucherProgram foodVoucherProgram);
 
-    /// @notice Emitted when an foodVoucherProgram is successfully claimed
-    /// @param receiver The address that received the foodVoucherProgram
-    event FoodVoucherProgramClaimed(uint256 indexed foodVoucherProgramId, address receiver);
+    // /// @notice Emitted when an foodVoucherProgram is successfully claimed
+    // /// @param receiver The address that received the foodVoucherProgram
+    // event FoodVoucherProgramClaimed(uint256 indexed foodVoucherProgramId, address receiver);
 
-    /// @notice Emitted when the foodVoucherProgramped amount is changed
-    /// @param foodVoucherProgramId The id of the foodVoucherProgram getting updated
-    /// @param foodVoucherProgram The new details for the foodVoucherProgram
-    event FoodVoucherProgramUpdated(uint256 indexed foodVoucherProgramId, FoodVoucherProgram foodVoucherProgram);
+    // /// @notice Emitted when the foodVoucherProgramped amount is changed
+    // /// @param foodVoucherProgramId The id of the foodVoucherProgram getting updated
+    // /// @param foodVoucherProgram The new details for the foodVoucherProgram
+    // event FoodVoucherProgramUpdated(uint256 indexed foodVoucherProgramId, FoodVoucherProgram foodVoucherProgram);
 
 
     ///////////////////////////////////////////////////////////////////////////////
     ///                                 STRUCTS                                ///
     //////////////////////////////////////////////////////////////////////////////
 
-    /// @notice Stores the details for a specific foodVoucherProgram
-    /// @param groupId The ID of the Semaphore group that will be eligible to claim this foodVoucherProgram
-    /// @param token The ERC20 token that will be foodVoucherProgramped to eligible participants
-    /// @param manager The address that manages this foodVoucherProgram, which is allowed to update the foodVoucherProgram details.
-    /// @param holder The address holding the tokens that will be foodVoucherProgramped
-    /// @param amount The amount of tokens that each participant will receive upon claiming
-    struct FoodVoucherProgram {
-        uint256 groupId;
-        FoodVoucherNFT token;
-        address manager;
-        address holder;
-        uint256 tokenId;  //@dev - Token ID of FoodVoucherNFT
-        //uint256 amount;
-    }
+    // /// @notice Stores the details for a specific foodVoucherProgram
+    // /// @param groupId The ID of the Semaphore group that will be eligible to claim this foodVoucherProgram
+    // /// @param token The ERC20 token that will be foodVoucherProgramped to eligible participants
+    // /// @param manager The address that manages this foodVoucherProgram, which is allowed to update the foodVoucherProgram details.
+    // /// @param holder The address holding the tokens that will be foodVoucherProgramped
+    // /// @param amount The amount of tokens that each participant will receive upon claiming
+    // struct FoodVoucherProgram {
+    //     uint256 groupId;
+    //     FoodVoucherNFT token;
+    //     address manager;
+    //     address holder;
+    //     uint256 tokenId;  //@dev - Token ID of FoodVoucherNFT
+    //     //uint256 amount;
+    // }
 
 
     ///////////////////////////////////////////////////////////////////////////////
@@ -111,7 +113,7 @@ contract WorldIdGatedVoucher {
         address holder,
         uint256 tokenId
         //uint256 amount
-    ) public {
+    ) public override {
         FoodVoucherProgram memory foodVoucherProgram = FoodVoucherProgram({
             groupId: groupId,
             token: token,
@@ -144,7 +146,7 @@ contract WorldIdGatedVoucher {
         uint256 root,
         uint256 nullifierHash,
         uint256[8] calldata proof
-    ) public {
+    ) public override {
         //@dev - first, we make sure this person hasn't done this before
         if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
 
@@ -181,7 +183,7 @@ contract WorldIdGatedVoucher {
     /// @notice Update the details for a given foodVoucherProgram, for addresses that haven't claimed already. Can only be called by the foodVoucherProgram creator
     /// @param foodVoucherProgramId The id of the foodVoucherProgram to update
     /// @param foodVoucherProgram The new details for the foodVoucherProgram
-    function updateDetails(uint256 foodVoucherProgramId, FoodVoucherProgram calldata foodVoucherProgram) public {
+    function updateDetails(uint256 foodVoucherProgramId, FoodVoucherProgram calldata foodVoucherProgram) public override {
         if (getFoodVoucherPrograms[foodVoucherProgramId].manager != msg.sender) revert Unauthorized();
 
         getFoodVoucherPrograms[foodVoucherProgramId] = foodVoucherProgram;
