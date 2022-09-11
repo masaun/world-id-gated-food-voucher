3c0db6abf9c74e4b7dc27fef04cd847fd49c43de
masaun <masanoriuno@gmail.com>
2022-09-11 18:12:44 +0900

    [SC]: Remove code of Structs, Events, Erros from the WorldIdGatedVoucher.sol

--- contracts/WorldIdGatedVoucher.sol
+++ contracts/WorldIdGatedVoucher.sol
@@ -21,58 +21,6 @@ import { Errors } from './libraries/Errors.sol';
 contract WorldIdGatedVoucher is IWorldIdGatedVoucher {
     using ByteHasher for bytes;
 
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  ERRORS                                ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    // /// @notice Thrown when trying to create or update FoodVoucherProgram details without being the manager
-    // error Unauthorized();
-
-    // /// @notice Thrown when attempting to reuse a nullifier
-    // error InvalidNullifier();
-
-    // /// @notice Thrown when attempting to claim a non-existant FoodVoucherProgram
-    // error InvalidFoodVoucherProgram();
-
-
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  EVENTS                                ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    // /// @notice Emitted when an FoodVoucherProgram is created
-    // /// @param foodVoucherProgramId The id of the foodVoucherProgram
-    // /// @param foodVoucherProgram The foodVoucherProgram details
-    // event FoodVoucherProgramCreated(uint256 foodVoucherProgramId, FoodVoucherProgram foodVoucherProgram);
-
-    // /// @notice Emitted when an foodVoucherProgram is successfully claimed
-    // /// @param receiver The address that received the foodVoucherProgram
-    // event FoodVoucherProgramClaimed(uint256 indexed foodVoucherProgramId, address receiver);
-
-    // /// @notice Emitted when the foodVoucherProgramped amount is changed
-    // /// @param foodVoucherProgramId The id of the foodVoucherProgram getting updated
-    // /// @param foodVoucherProgram The new details for the foodVoucherProgram
-    // event FoodVoucherProgramUpdated(uint256 indexed foodVoucherProgramId, FoodVoucherProgram foodVoucherProgram);
-
-
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                 STRUCTS                                ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    // /// @notice Stores the details for a specific foodVoucherProgram
-    // /// @param groupId The ID of the Semaphore group that will be eligible to claim this foodVoucherProgram
-    // /// @param token The ERC20 token that will be foodVoucherProgramped to eligible participants
-    // /// @param manager The address that manages this foodVoucherProgram, which is allowed to update the foodVoucherProgram details.
-    // /// @param holder The address holding the tokens that will be foodVoucherProgramped
-    // /// @param amount The amount of tokens that each participant will receive upon claiming
-    // struct FoodVoucherProgram {
-    //     uint256 groupId;
-    //     FoodVoucherNFT token;
-    //     address manager;
-    //     address holder;
-    //     uint256 tokenId;  //@dev - Token ID of FoodVoucherNFT
-    //     //uint256 amount;
-    // }
-
 
     ///////////////////////////////////////////////////////////////////////////////
     ///                              CONFIG STORAGE                            ///
