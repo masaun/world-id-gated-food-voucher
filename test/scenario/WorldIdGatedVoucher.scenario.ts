e899f315ca63c28fb81ce9bf9fcf1c4004fcc08b
masaun <masanoriuno@gmail.com>
2022-09-14 18:15:23 +0900

    [Scenario Test]: Replace arg value of getProof() from 'callerAddr' to 'REFUGEE'

--- test/scenario/WorldIdGatedVoucher.scenario.ts
+++ test/scenario/WorldIdGatedVoucher.scenario.ts
@@ -88,7 +88,7 @@ describe('Scenario test - WorldIdGatedVoucher\n', function () {
         let tx1 = await foodVoucherNFT.connect(deployer).mintFoodVoucherNFT(ISSUER)
     })
 
-    it('Check FoodVoucherNFT balance of each wallet addresses before claiming - Issuer should has a FoodVoucherNFT. Refugee should not has a FoodVoucherNFT', async function () {
+    it('Check FoodVoucherNFT balance of each wallet addresses before claiming - Issuer should has a FoodVoucherNFT. Refugee should not has a FoodVoucherNFT\n', async function () {
         let foodVoucherNFTBalanceOfDeployer = await foodVoucherNFT.balanceOf(DEPLOYER)
         let foodVoucherNFTBalanceOfIssuer = await foodVoucherNFT.balanceOf(ISSUER)
         let foodVoucherNFTBalanceOfrefugee = await foodVoucherNFT.balanceOf(REFUGEE)
@@ -100,7 +100,7 @@ describe('Scenario test - WorldIdGatedVoucher\n', function () {
         expect(foodVoucherNFTBalanceOfrefugee).to.equal(0)
     })
 
-    it('createFoodVoucherProgram() - An issuer create a FoodVoucherProgram / Then, claimFoodVoucher() - refugee claim a FoodVoucherNFT', async function () {
+    it('createFoodVoucherProgram() - Issuer create a FoodVoucherProgram / Then, claimFoodVoucher() - Refugee claim a FoodVoucherNFT', async function () {
         //@dev - Create a new FoodVoucherProgram
         const groupId = 1
         const token = FOOD_VOUCHER_NFT
@@ -115,7 +115,7 @@ describe('Scenario test - WorldIdGatedVoucher\n', function () {
         await registerIdentity()
 
         //@dev - get proof
-        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, callerAddr)
+        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, REFUGEE)
 
         //@dev - A issuer approve the WorldIdGatedVoucher contract to spend tokenID of FoodVoucherNFT
         let tx2 = await foodVoucherNFT.connect(issuer).approve(WORLD_ID_GATED_VOUCHER, tokenId);
