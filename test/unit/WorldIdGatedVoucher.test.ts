6ad05f1ce2e1720dab12f542bbc6b2e42c3ec872
masaun <masanoriuno@gmail.com>
2022-09-15 11:27:23 +0900

    [Unit test]: Fix unit test by following scenario test

--- test/unit/WorldIdGatedVoucher.test.ts
+++ test/unit/WorldIdGatedVoucher.test.ts
@@ -63,43 +63,41 @@ describe('Unit test - WorldIdGatedVoucher', function () {
         REFUGEE = refugee.address
         console.log(`Wallet address of deployer: ${ DEPLOYER }`)
         console.log(`Wallet address of issuer: ${ ISSUER }`)
-        console.log(`Wallet address of refugee: ${ REFUGEE }`)
+        console.log(`Wallet address of refugee: ${ REFUGEE }\n`)
         
         const worldIDAddress = await setUpWorldID()
-        console.log(`WorldID deployed-address: ${ worldIDAddress }`)
+        console.log(`WorldID deployed-address: ${ worldIDAddress }\n`)
 
         //@dev - Deploy the WorldIdGatedVoucher.sol
         const WorldIdGatedVoucher = await ethers.getContractFactory('WorldIdGatedVoucher')
         worldIdGatedVoucher = await WorldIdGatedVoucher.deploy(worldIDAddress)
         WORLD_ID_GATED_VOUCHER = worldIdGatedVoucher.address
-        console.log(`Deployed-address of the WorldIdGatedVoucher.sol: ${ WORLD_ID_GATED_VOUCHER }`)
+        console.log(`\nDeployed-address of the WorldIdGatedVoucher.sol: ${ WORLD_ID_GATED_VOUCHER }\n`)
         await worldIdGatedVoucher.deployed()
 
-        //@dev - [TODO]: At the end of implementation, this deployment part should be replaced with a method defined in the FoodVoucherNFTFactory.sol
         //@dev - Deploy a FoodVoucherNFT.sol
         const FoodVoucherNFT = await ethers.getContractFactory('FoodVoucherNFT')
-        foodVoucherNFT = await FoodVoucherNFT.deploy()
+        foodVoucherNFT = await FoodVoucherNFT.deploy(ISSUER)
         FOOD_VOUCHER_NFT = foodVoucherNFT.address
-        console.log(`Deployed-address of the FoodVoucherNFT.sol: ${ FOOD_VOUCHER_NFT }`)
+        console.log(`Deployed-address of the FoodVoucherNFT.sol: ${ FOOD_VOUCHER_NFT }\n`)
         await foodVoucherNFT.deployed()
 
         //@dev - Mint a FoodVoucherNFT (tokenID=0)
-        let tx = await foodVoucherNFT.connect(deployer).mintFoodVoucherNFT(issuer.address)
+        let tx = await foodVoucherNFT.connect(issuer).mintFoodVoucherNFT(ISSUER)
 
         //@dev - Assign a caller address (that is a claimer address)
-        callerAddr = REFUGEE
-        //callerAddr = await deployer.getAddress()
-        console.log(`callerAddr: ${ callerAddr }`)
+        //callerAddr = REFUGEE
+        //console.log(`callerAddr: ${ callerAddr }`)
     })
 
     it('Check FoodVoucherNFT balance of each wallet addresses before claiming', async function () {
-        let foodVoucherNFTBalanceOfDeployer = await foodVoucherNFT.balanceOf(DEPLOYER)
+        //let foodVoucherNFTBalanceOfDeployer = await foodVoucherNFT.balanceOf(DEPLOYER)
         let foodVoucherNFTBalanceOfIssuer = await foodVoucherNFT.balanceOf(ISSUER)
         let foodVoucherNFTBalanceOfrefugee = await foodVoucherNFT.balanceOf(REFUGEE)
-        console.log(`##### FoodVoucherNFT balance of deployer: ${ foodVoucherNFTBalanceOfDeployer } #####`)
-        console.log(`##### FoodVoucherNFT balance of issuer: ${ foodVoucherNFTBalanceOfIssuer } #####`)
+        //console.log(`\n##### FoodVoucherNFT balance of deployer: ${ foodVoucherNFTBalanceOfDeployer } #####`)
+        console.log(`\n##### FoodVoucherNFT balance of issuer: ${ foodVoucherNFTBalanceOfIssuer } #####`)
         console.log(`##### FoodVoucherNFT balance of refugee: ${ foodVoucherNFTBalanceOfrefugee } #####`)
-        expect(foodVoucherNFTBalanceOfDeployer).to.equal(0)
+        //expect(foodVoucherNFTBalanceOfDeployer).to.equal(0)
         expect(foodVoucherNFTBalanceOfIssuer).to.equal(1)
         expect(foodVoucherNFTBalanceOfrefugee).to.equal(0)
     })
@@ -114,19 +112,19 @@ describe('Unit test - WorldIdGatedVoucher', function () {
         //let txReceipt = await tx1.wait()
         //console.log(`txReceipt of worldIdGatedVoucher#createFoodVoucherProgram(): ${ JSON.stringify(txReceipt, null, 2) }`)
 
-        //[TODO]: Get foodVoucherProgramId via SC
+        //@dev - Assign Food Voucher Program Id
         let foodVoucherProgramId: number = 1
 
         await registerIdentity()
 
-        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, callerAddr)
+        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, REFUGEE)
 
         //@dev - A issuer approve the WorldIdGatedVoucher contract to spend tokenID of FoodVoucherNFT
         let tx2 = await foodVoucherNFT.connect(issuer).approve(WORLD_ID_GATED_VOUCHER, tokenId);
 
         const tx3 = await worldIdGatedVoucher.connect(refugee).claimFoodVoucher(
             foodVoucherProgramId, 
-            callerAddr,               // receiver
+            REFUGEE,  // receiver
             await getRoot(),
             nullifierHash,
             proof
@@ -137,20 +135,13 @@ describe('Unit test - WorldIdGatedVoucher', function () {
         //@dev - Extra checks here
 
         //@dev - Check FoodVoucherNFT balance of each wallet addresses
-        //const owner = callerAddr
-        //const tokenId = 0
-        //let owner = await foodVoucherNFT.ownerOf(tokenId)
-        //let foodVoucherNFTBalance = await foodVoucherNFT.balanceOf(owner)
-        //console.log(`##### FoodVoucherNFT balance of ${ owner }: ${ foodVoucherNFTBalance } #####`)
-        //assertEq(token.balanceOf(address(this)), 1 ether);
-
-        let foodVoucherNFTBalanceOfDeployer = await foodVoucherNFT.balanceOf(DEPLOYER)
+        //let foodVoucherNFTBalanceOfDeployer = await foodVoucherNFT.balanceOf(DEPLOYER)
         let foodVoucherNFTBalanceOfIssuer = await foodVoucherNFT.balanceOf(ISSUER)
         let foodVoucherNFTBalanceOfrefugee = await foodVoucherNFT.balanceOf(REFUGEE)
-        console.log(`##### FoodVoucherNFT balance of deployer: ${ foodVoucherNFTBalanceOfDeployer } #####`)
-        console.log(`##### FoodVoucherNFT balance of issuer: ${ foodVoucherNFTBalanceOfIssuer } #####`)
+        //console.log(`\n##### FoodVoucherNFT balance of deployer: ${ foodVoucherNFTBalanceOfDeployer } #####`)
+        console.log(`\n##### FoodVoucherNFT balance of issuer: ${ foodVoucherNFTBalanceOfIssuer } #####`)
         console.log(`##### FoodVoucherNFT balance of refugee: ${ foodVoucherNFTBalanceOfrefugee } #####`)
-        expect(foodVoucherNFTBalanceOfDeployer).to.equal(0)
+        //expect(foodVoucherNFTBalanceOfDeployer).to.equal(0)
         expect(foodVoucherNFTBalanceOfIssuer).to.equal(0)
         expect(foodVoucherNFTBalanceOfrefugee).to.equal(1)
     })
@@ -158,26 +149,26 @@ describe('Unit test - WorldIdGatedVoucher', function () {
     it('Rejects duplicated calls', async function () {
         //@dev - Create a new FoodVoucherProgram
         const groupId = 1
-        const token = FOOD_VOUCHER_NFT  // Food Voucher NFT
+        const token = FOOD_VOUCHER_NFT
         const holder = issuer.address
         const tokenId = 0  //@dev - TokenID of FoodVoucherNFTs
         let tx1 = await worldIdGatedVoucher.connect(issuer).createFoodVoucherProgram(groupId, token, holder, tokenId)
         //let txReceipt = await tx1.wait()
         //console.log(`txReceipt of worldIdGatedVoucher#createFoodVoucherProgram(): ${ JSON.stringify(txReceipt, null, 2) }`)
 
-        //[TODO]: Get foodVoucherProgramId via SC
+        //@dev - Assign Food Voucher Program Id
         let foodVoucherProgramId: number = 1
 
         await registerIdentity()
 
-        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, callerAddr)
+        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, REFUGEE)
 
         //@dev - A issuer approve the WorldIdGatedVoucher contract to spend tokenID of FoodVoucherNFT
         let tx2 = await foodVoucherNFT.connect(issuer).approve(WORLD_ID_GATED_VOUCHER, tokenId);
 
         const tx3 = await worldIdGatedVoucher.connect(refugee).claimFoodVoucher(
             foodVoucherProgramId, 
-            callerAddr,               // receiver
+            REFUGEE,  // receiver
             await getRoot(),
             nullifierHash,
             proof
@@ -186,7 +177,7 @@ describe('Unit test - WorldIdGatedVoucher', function () {
         await tx3.wait()
 
         await expect(
-            worldIdGatedVoucher.claimFoodVoucher(foodVoucherProgramId, callerAddr, await getRoot(), nullifierHash, proof)
+            worldIdGatedVoucher.claimFoodVoucher(foodVoucherProgramId, REFUGEE, await getRoot(), nullifierHash, proof)
         ).to.be.revertedWith('InvalidNullifier')
 
         // extra checks here
@@ -206,13 +197,11 @@ describe('Unit test - WorldIdGatedVoucher', function () {
 
         await registerInvalidIdentity()
 
-        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, callerAddr)
+        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, REFUGEE)
 
         await expect(
-            worldIdGatedVoucher.claimFoodVoucher(foodVoucherProgramId, callerAddr, await getRoot(), nullifierHash, proof)
+            worldIdGatedVoucher.claimFoodVoucher(foodVoucherProgramId, REFUGEE, await getRoot(), nullifierHash, proof)
         ).to.be.revertedWith('InvalidProof')
-
-        // extra checks here
     })
 
     it('Rejects calls with an invalid signal', async function () {
@@ -229,7 +218,7 @@ describe('Unit test - WorldIdGatedVoucher', function () {
         
         await registerIdentity()
 
-        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, callerAddr)
+        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, REFUGEE)
 
         await expect(
             worldIdGatedVoucher.claimFoodVoucher(foodVoucherProgramId, WORLD_ID_GATED_VOUCHER, await getRoot(), nullifierHash, proof)
@@ -252,11 +241,11 @@ describe('Unit test - WorldIdGatedVoucher', function () {
 
         await registerIdentity()
 
-        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, callerAddr)
+        const [nullifierHash, proof] = await getProof(WORLD_ID_GATED_VOUCHER, REFUGEE)
         proof[0] = (BigInt(proof[0]) ^ BigInt(42)).toString()
 
         await expect(
-            worldIdGatedVoucher.claimFoodVoucher(foodVoucherProgramId, callerAddr, await getRoot(), nullifierHash, proof)
+            worldIdGatedVoucher.claimFoodVoucher(foodVoucherProgramId, REFUGEE, await getRoot(), nullifierHash, proof)
         ).to.be.revertedWith('InvalidProof')
 
         // extra checks here
